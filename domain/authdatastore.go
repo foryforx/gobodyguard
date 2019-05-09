b7cf9a24e7558e545d4c46973d2aafb73f3992cd
K.AL <kal@checkbox.ai>
2019-05-09 11:35:41 +1000

    go auth service and repository

--- domain/authdatastore.go
+++ domain/authdatastore.go
@@ -7,31 +7,28 @@ import (
 // AuthDataStore defines operations expected from data storage entity
 type AuthDataStore interface {
 	// Principal
-	GetPrincipal(uuid string) (models.Principal, error)
+	GetPrincipal(UUID string) (models.Principal, error)
 	AddPrincipal(principal models.Principal) (models.Principal, error)
 	UpdatePrincipal(principal models.Principal) (models.Principal, error)
-	DeletePrincipal(uuid string) error
+	DeletePrincipal(UUID string) error
 
 	// Resource
-	GetResource(uuid string) (models.Resource, error)
+	GetResource(UUID string) (models.Resource, error)
 	AddResource(resource models.Resource) (models.Resource, error)
 	UpdateResource(resource models.Resource) (models.Resource, error)
-	DeleteResource(uuid string) error
+	DeleteResource(UUID string) error
 
 	// Operation
-	GetOperation(uuid string) (models.Operation, error)
+	GetOperation(UUID string) (models.Operation, error)
 	AddOperation(operation models.Operation) (models.Operation, error)
 	UpdateOperation(operation models.Operation) (models.Operation, error)
-	DeleteOperation(uuid string) error
+	DeleteOperation(UUID string) error
 
 	// Policy
-	GetPolicy(uuid string) (models.Policy, error)
+	GetPolicy(UUID string) (models.Policy, error)
+	GetPolicyForAllMatch(principalUUID string, resourceUUID string,
+		operationUUID string) ([]models.Policy, error)
 	AddPolicy(policy models.Policy) (models.Policy, error)
 	UpdatePolicy(policy models.Policy) (models.Policy, error)
-	DeletePolicy(uuid string) error
-
-	// Tags
-	AddTagsToPolicy(uuid string, tags []string) error
-	RemoveTagsFromPolicy(uuid string, tag string) error
-	SearchForTagInPolicy(tag []string) error
+	DeletePolicy(UUID string) error
 }
--- repository/authdatastorage.go
+++ repository/authdatastorage.go
@@ -12,7 +12,7 @@ type authStorage struct {
 }
 
 // NewWorkflowRepository To create new Repository with connection to DB
-func NewWorkflowRepository(conn *gorm.DB) *AuthDataStore {
+func NewWorkflowRepository(conn *gorm.DB) *domain.AuthDataStore {
 	return &authStorage{conn}
 }
 
@@ -20,96 +20,148 @@ func (a *authStorage) GetConnection() *gorm.DB {
 	return a.Conn
 }
 
-// Principal
-func (a *authStorage) GetPrincipal(uuid string) (models.Principal, error) {
-	if len(uuid) <= 10 {
-		return errors.Errorf("UUID not valid:%v", uuid)
-	}
+// GetPrincipal will get a principal data from datastore for a particular uuid
+func (a *authStorage) GetPrincipal(UUID string) (models.Principal, error) {
 	var principal models.Principal
-	err := a.Conn.Where("uuid = ?", uuid).Find(&principal).Error
+	if len(UUID) <= 10 {
+		return principal, errors.Errorf("UUID not valid:%v", UUID)
+	}
+	err := a.Conn.Where("uuid = ?", UUID).Find(&principal).Error
 	if err != nil && err != gorm.ErrRecordNotFound {
-		return principal, errors.Errorf(err, "GetPrincipal: error %v", uuid)
+		return principal, errors.Wrapf(err, "GetPrincipal: error %v", UUID)
 	}
 	return principal, nil
 }
+
+// AddPrincipal will add a principal data to datastore
 func (a *authStorage) AddPrincipal(principal models.Principal) (models.Principal, error) {
 	err := a.Conn.Save(&principal).Error
 	if err != nil {
-		return principal, errors.Errorf(err, "AddPrincipal error %v", principal)
+		return principal, errors.Wrapf(err, "AddPrincipal error %v", principal)
 	}
 	return principal, nil
 }
+
+// UpdatePrincipal will update the respective principal data in datastore
 func (a *authStorage) UpdatePrincipal(principal models.Principal) (models.Principal, error) {
-
+	err := a.Conn.Update(&principal).Error
+	if err != nil {
+		return principal, errors.Wrapf(err, "UpdatePrincipal error %v", principal)
+	}
+	return principal, nil
 }
-func (a *authStorage) DeletePrincipal(uuid string) error {
 
+// DeletePrincipal will remove the principal data from data store
+func (a *authStorage) DeletePrincipal(UUID string) error {
+	err := a.Conn.Unscoped().Where("uuid::text = ?", UUID).Delete(&models.Principal{}).Error
+	return err
 }
 
-// Resource
-func (a *authStorage) GetResource(uuid string) (models.Resource, error) {
-	if len(uuid) <= 10 {
-		return errors.Errorf("UUID not valid:%v", uuid)
+// GetResource will get the resource data from data store for the UUID
+func (a *authStorage) GetResource(UUID string) (models.Resource, error) {
+	if len(UUID) <= 10 {
+		return errors.Errorf("UUID not valid:%v", UUID)
 	}
 	var resource models.Principal
-	err := a.Conn.Where("uuid = ?", uuid).Find(&resource).Error
+	err := a.Conn.Where("uuid = ?", UUID).Find(&resource).Error
 	if err != nil && err != gorm.ErrRecordNotFound {
-		return resource, errors.Errorf(err, "GetResource: error %v", uuid)
+		return resource, errors.Wrapf(err, "GetResource: error %v", UUID)
 	}
 	return resource, nil
 }
+
+// AddResource will add a new resource to data store
 func (a *authStorage) AddResource(resource models.Resource) (models.Resource, error) {
 	err := a.Conn.Save(&resource).Error
 	if err != nil {
-		return resource, errors.Errorf(err, "AddResource error %v", resource)
+		return resource, errors.Wrapf(err, "AddResource error %v", resource)
 	}
 	return resource, nil
 }
+
+// UpdateResource will update the existing resource in data store
 func (a *authStorage) UpdateResource(resource models.Resource) (models.Resource, error) {
-
+	err := a.Conn.Update(&resource).Error
+	if err != nil {
+		return resource, errors.Wrapf(err, "UpdateResource error %v", resource)
+	}
+	return resource, nil
 }
-func (a *authStorage) DeleteResource(uuid string) error {
 
+// DeleteResource will delete the existing resource from data store
+func (a *authStorage) DeleteResource(UUID string) error {
+	err := a.Conn.Unscoped().Where("uuid::text = ?", UUID).Delete(&models.Resource{}).Error
+	return err
 }
 
-// Operation
-func (a *authStorage) GetOperation(uuid string) (models.Operation, error) {
-	if len(uuid) <= 10 {
-		return errors.Errorf("UUID not valid:%v", uuid)
+// GetOperation will the operation data from data store for UUID
+func (a *authStorage) GetOperation(UUID string) (models.Operation, error) {
+	if len(UUID) <= 10 {
+		return errors.Errorf("UUID not valid:%v", UUID)
 	}
 	var operation models.Operation
-	err := a.Conn.Where("uuid = ?", uuid).Find(&operation).Error
+	err := a.Conn.Where("uuid = ?", UUID).Find(&operation).Error
 	if err != nil && err != gorm.ErrRecordNotFound {
-		return operation, errors.Errorf(err, "GetOperation: error %v", uuid)
+		return operation, errors.Wrapf(err, "GetOperation: error %v", UUID)
 	}
 	return operation, nil
 }
+
+// AddOperation will add a new operation to data store
 func (a *authStorage) AddOperation(operation models.Operation) (models.Operation, error) {
 	err := a.Conn.Save(&operation).Error
 	if err != nil {
-		return operation, errors.Errorf(err, "AddOperation error %v", operation)
+		return operation, errors.Wrapf(err, "AddOperation error %v", operation)
 	}
 	return operation, nil
 }
+
+// UpdateOperation will update the existing operation from data store
 func (a *authStorage) UpdateOperation(operation models.Operation) (models.Operation, error) {
-
+	err := a.Conn.Update(&operation).Error
+	if err != nil {
+		return operation, errors.Wrapf(err, "UpdateOperation error %v", operation)
+	}
+	return operation, nil
 }
-func (a *authStorage) DeleteOperation(uuid string) error {
 
+// DeleteOperation will delete the operation from data store
+func (a *authStorage) DeleteOperation(UUID string) error {
+	err := a.Conn.Unscoped().Where("uuid::text = ?", UUID).Delete(&models.Operation{}).Error
+	return err
 }
 
-// Policy
-func (a *authStorage) GetPolicy(uuid string) (models.Policy, error) {
-	if len(uuid) <= 10 {
-		return errors.Errorf("UUID not valid:%v", uuid)
+// GetPolicy will get the policy data from data store for UUID
+func (a *authStorage) GetPolicy(UUID string) (models.Policy, error) {
+	if len(UUID) <= 10 {
+		return errors.Errorf("UUID not valid:%v", UUID)
 	}
 	var policy models.Policy
-	err := a.Conn.Where("uuid = ?", uuid).Find(&policy).Error
+	err := a.Conn.Where("uuid = ?", UUID).Find(&policy).Error
 	if err != nil && err != gorm.ErrRecordNotFound {
-		return policy, errors.Errorf(err, "GetPolicy: error %v", uuid)
+		return policy, errors.Errorf(err, "GetPolicy: error %v", UUID)
 	}
 	return policy, nil
 }
+
+// GetPolicyForAllMatch will get the policy from data store which matches principal, operation and resource UUID
+func (a *authStorage) GetPolicyForAllMatch(principalUUID string, resourceUUID string,
+	operationUUID string) ([]models.Policy, error) {
+	if len(principalUUID) <= 10 || len(resourceUUID) <= 10 || len(operationUUID) <= 10 {
+		return errors.Errorf("UUID's not valid:%v %v %v", principalUUID, resourceUUID, operationUUID)
+	}
+	var policys = make([]models.Policy, 0)
+	err := a.Conn.Where("principal_uuid = ? AND resource_uuid = ? AND operation_UUID = ?",
+		principalUUID,
+		resourceUUID,
+		operationUUID).Find(&policys).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return policys, errors.Errorf(err, "GetPolicy: error %v", UUID)
+	}
+	return policys, nil
+}
+
+// AddPolicy will add a new policy to data store
 func (a *authStorage) AddPolicy(policy models.Policy) (models.Policy, error) {
 	err := a.Conn.Save(&policy).Error
 	if err != nil {
@@ -117,20 +169,18 @@ func (a *authStorage) AddPolicy(policy models.Policy) (models.Policy, error) {
 	}
 	return policy, nil
 }
+
+// UpdatePolicy will update the existing policy from data store
 func (a *authStorage) UpdatePolicy(policy models.Policy) (models.Policy, error) {
-
+	err := a.Conn.Update(&policy).Error
+	if err != nil {
+		return policy, errors.Wrapf(err, "UpdatePolicy error %v", policy)
+	}
+	return policy, nil
 }
-func (a *authStorage) DeletePolicy(uuid string) error {
 
-}
-
-// Tags
-func (a *authStorage) AddTagsToPolicy(uuid string, tags []string) error {
-
-}
-func (a *authStorage) RemoveTagsFromPolicy(uuid string, tag string) error {
-
-}
-func (a *authStorage) SearchForTagInPolicy(tag []string) error {
-
+// DeletePolicy will delete the existing policy from data store
+func (a *authStorage) DeletePolicy(UUID string) error {
+	err := a.Conn.Unscoped().Where("uuid::text = ?", UUID).Delete(&models.Policy{}).Error
+	return err
 }
